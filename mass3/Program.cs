99894e6e02bff5614509d9f3fe7f14269dfd4396
Petr <sppetrak@mail.ru>
2023-01-06 13:39:25 +0300

    программа написана и проврена

--- mass3/Program.cs
+++ mass3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 5 строк массива через пробел: ");
+string[] array1 = new string[5];
+array1 = Console.ReadLine()!.Split( ); 
+
+string[] array2 = new string[array1.Length];
+
+
+void stringlenght(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+stringlenght(array1, array2);
+PrintArray(array2);
